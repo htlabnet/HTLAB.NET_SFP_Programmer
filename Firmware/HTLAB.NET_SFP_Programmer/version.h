7ae47589dba4d64d37b45a8b29915d2de4bf433e
GitHub Action <action@github.com>
2020-08-07 10:21:09 +0900

    Add Version Number

---
+++ Firmware/HTLAB.NET_SFP_Programmer/version.h
@@ -0,0 +1,14 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VER_RUN_ID 198529889
+#define VER_RUN_NUM 1
+#define VER_BRANCH master
+#define VER_YEAR 20
+#define VER_MON 08
+#define VER_DATE 07
+#define VER_HOUR 10
+#define VER_MIN 21
+#define VER_SEC 08
+
+#endif
